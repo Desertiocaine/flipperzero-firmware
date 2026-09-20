7713fccc91f8abcd71d2f4e1deb7b59dc44b6229
Desertiocaine <desertiocaine@gmail.com>
2026-09-20 18:10:26 -0500

    Set the Flipper BLE/local device name to Packwhacker without xFlipper prefix

--- targets/f7/furi_hal/furi_hal_version.c
+++ targets/f7/furi_hal/furi_hal_version.c
@@ -97,7 +97,7 @@ static void furi_hal_version_set_name(const char* name) {
     snprintf(
         furi_hal_version.device_name,
         FURI_HAL_VERSION_DEVICE_NAME_LENGTH,
-        "xFlipper %s",
+        "%s",
         custom_name);
 
     furi_hal_version.device_name[0] = AD_TYPE_COMPLETE_LOCAL_NAME;
